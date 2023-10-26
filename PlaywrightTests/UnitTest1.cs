4662893a8c105896319cc9dc0ae8570ebc2c54e2
silvanesa2023 <silvana.lapizondo@gmail.com>
2023-10-26 16:33:35 -0300

    started with playwright

---
+++ PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.Playwright;
+
+namespace PlaywrightTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://qa-navigator.creditunionsfirst.org/");
+        // await page.ClickAsync(selector:"label=Sign In");
+        await page.ScreenshotAsync(new PageScreenshotOptions{
+                Path = "EAApp1.jpg"
+
+            });
+        await page.FillAsync(selector:"#email", value:"admin");
+        await page.FillAsync(selector:"#password", value:"Password1");
+        await page.ClickAsync(selector:"text=Sign In");
+        var isExist = await page.Locator(selector:"text=Dashboard").IsVisibleAsync();
+    }
+}
